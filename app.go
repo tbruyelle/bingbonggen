b4f0d4246752cf063d3645711e34f72463cb8999
Thomas Bruyelle <thomas.bruyelle@keeneye.tech>
2018-01-14 18:51:40 +0100

    It works

--- app.go
+++ app.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/event"
 	"github.com/gopherjs/vecty/prop"
 )
 
@@ -12,16 +15,47 @@ func main() {
 
 type App struct {
 	vecty.Core
+
+	state struct {
+		body  string
+		eyes  string
+		mouth string
+		bonus string
+	}
 }
 
-func staticPath(s string) string {
-	return "file:///home/thomas/src/bingbong/" + s
+func (c *App) body() *vecty.HTML {
+	if c.state.body == "" {
+		c.state.body = "body_base"
+	}
+	return img(c.state.body)
+}
+
+func (c *App) eyes() *vecty.HTML {
+	if c.state.eyes == "" {
+		c.state.eyes = "eyes_base"
+	}
+	return img(c.state.eyes)
+}
+
+func (c *App) mouth() *vecty.HTML {
+	if c.state.mouth == "" {
+		c.state.mouth = "mouth_base"
+	}
+	return img(c.state.mouth)
+}
+
+func (c *App) bonus() *vecty.HTML {
+	if c.state.bonus == "" {
+		c.state.bonus = "empty"
+	}
+	return img(c.state.bonus)
 }
 
 func img(s string) *vecty.HTML {
 	return elem.Image(
 		vecty.Markup(
-			prop.Src(staticPath("assets/"+s+".png")),
+			prop.Src("/assets/"+s+".png"),
 			vecty.Style("position", "absolute"),
 			vecty.Style("top", "0px"),
 			vecty.Style("width", "100%"),
@@ -32,21 +66,97 @@ func img(s string) *vecty.HTML {
 func (c *App) Render() vecty.ComponentOrHTML {
 	vecty.SetTitle("Générateur de BingBong")
 	return elem.Body(
+		vecty.Markup(vecty.Style("padding-top", "30px")),
 		elem.Div(
 			vecty.Markup(vecty.Class("container")),
 			elem.Div(
 				vecty.Markup(vecty.Class("row")),
 				elem.Div(
 					vecty.Markup(vecty.Class("col-md-8")),
-					img("body_base"),
-					img("mouth_base"),
-					img("eyes_base"),
+					c.body(),
+					c.eyes(),
+					c.mouth(),
+					c.bonus(),
 				),
 				elem.Div(
 					vecty.Markup(vecty.Class("col-md-4")),
-					vecty.Text("Outils"),
+					elem.Button(
+						vecty.Markup(
+							vecty.Class("btn", "btn-default"),
+							event.Click(c.onReset),
+						),
+						vecty.Text("Reset"),
+					),
+					elem.Break(),
+					elem.Break(),
+					c.renderTools(),
 				),
 			),
 		),
 	)
+}
+
+func (c *App) onReset(*vecty.Event) {
+	c.state.body = ""
+	c.state.eyes = ""
+	c.state.bonus = ""
+	c.state.mouth = ""
+	vecty.Rerender(c)
+}
+
+func (c *App) onItemClick(item string) func(*vecty.Event) {
+	return func(*vecty.Event) {
+		print(item)
+		if strings.HasPrefix(item, "body") {
+			c.state.body = item
+		} else if strings.HasPrefix(item, "mouth") {
+			c.state.mouth = item
+		} else if strings.HasPrefix(item, "eyes") {
+			c.state.eyes = item
+		} else {
+			c.state.bonus = item
+		}
+		vecty.Rerender(c)
+	}
+}
+
+func (c *App) renderTools() *vecty.HTML {
+	return elem.Div(
+		c.renderTool("Corps", bodies),
+		c.renderTool("Yeux", eyes),
+		c.renderTool("Bouches", mouthes),
+	)
+}
+
+func (c *App) renderTool(title string, items []string) *vecty.HTML {
+	return elem.Div(
+		vecty.Markup(vecty.Class("card")),
+		elem.Div(
+			vecty.Markup(vecty.Class("card-body")),
+			elem.Heading5(
+				vecty.Markup(vecty.Class("card-title")),
+				vecty.Text(title),
+			),
+			c.renderItems(items),
+		),
+	)
+}
+
+func (c *App) renderItems(items []string) *vecty.HTML {
+	var list vecty.List
+
+	for _, item := range items {
+		list = append(list,
+			elem.Label(
+				vecty.Markup(vecty.Style("display", "inline")),
+				elem.Image(
+					vecty.Markup(
+						prop.Src("/assets/"+item+".png"),
+						vecty.Style("width", "20%"),
+						event.Click(c.onItemClick(item)),
+					),
+				),
+			))
+	}
+	return elem.Div(list)
 }
---
+++ items.go
@@ -0,0 +1,29 @@
+package main
+
+var bodies = []string{
+	"body_base",
+	"body_sick",
+	"body_angry",
+}
+
+var eyes = []string{
+	"eyes_base",
+	"eyes_girl",
+	"eyes_boy_tired",
+	"eyes_girl_tired",
+	"eyes_boy_angry",
+	"eyes_girl_angry",
+}
+
+var mouthes = []string{
+	"mouth_base",
+	"mouth_happy",
+	"mouth_o",
+	"mouth_smile",
+	"mouth_pascontent",
+	"mouth_miam",
+}
+
+var bonuses = []string{
+	"empty",
+}
